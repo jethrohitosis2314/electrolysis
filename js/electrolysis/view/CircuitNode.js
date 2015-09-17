66fa700decd00b408728b5f575543b2229b8810b
Abdulsattar Mohammed <asattar.md@gmail.com>
2015-09-17 17:55:12 +0530

    Fill the container with liquid

--- js/electrolysis/view/CircuitNode.js
+++ js/electrolysis/view/CircuitNode.js
@@ -3,11 +3,18 @@ define(function(require){
     var Node = require('SCENERY/nodes/Node');
     var inherit = require('PHET_CORE/inherit');
     var Image = require('SCENERY/nodes/Image');
+    var Rectangle = require('SCENERY/nodes/Rectangle');
 
     var circuitImage = require('image!ELECTROLYSIS/circuit.svg');
 
-    function CircuitNode(circuitModel, modelViewTransform) {
+    function CircuitNode(model, modelViewTransform) {
         Node.call(this, {x: 50, y: 100});
+
+        var rectangleNode = new Rectangle(200,300,150,100,0,0,{fill: '#000', lineWidth: 0});
+        this.addChild(rectangleNode);
+        model.electrolyteProperty.link(function(liquid) {
+            rectangleNode.fill = liquid ? liquid.color : 'transparent';
+        }.bind(this));
 
         var image = new Image(circuitImage, {x: 0, y: 0});
         this.addChild(image);
