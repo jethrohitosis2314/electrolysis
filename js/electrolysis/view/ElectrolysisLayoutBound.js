9a8e942ee021df71939a6b12e1f142aea9476a34
Prateek Srivastava <prateeks@thoughtworks.com>
2015-09-17 14:46:12 +0530

    initial commit

---
+++ js/electrolysis/view/ElectrolysisLayoutBound.js
@@ -0,0 +1,8 @@
+define( function( require ) {
+  'use strict';
+
+  // modules
+  var Bounds2 = require( 'DOT/Bounds2' );
+
+  return new Bounds2( 0, 0, 981, 604 );
+} );
--- js/electrolysis/view/ElectrolysisScreenView.js
+++ js/electrolysis/view/ElectrolysisScreenView.js
@@ -11,6 +11,9 @@ define( function( require ) {
   var inherit = require( 'PHET_CORE/inherit' );
   var ScreenView = require( 'JOIST/ScreenView' );
   var ResetAllButton = require( 'SCENERY_PHET/buttons/ResetAllButton' );
+  var Rectangle = require('SCENERY/nodes/Rectangle');
+  var ElectrolysisLayoutBound = require('ELECTROLYSIS/electrolysis/view/ElectrolysisLayoutBound');
+  var LinearGradient = require( 'SCENERY/util/LinearGradient' );
 
   /**
    * @param {ElectrolysisModel} electrolysisModel
@@ -20,6 +23,28 @@ define( function( require ) {
 
     ScreenView.call( this );
 
+
+    ScreenView.call( this, {
+      layoutBounds: ElectrolysisLayoutBound
+    } );
+
+    //Fit to the window and render the initial scene
+    var width = this.layoutBounds.width;
+    var height = this.layoutBounds.height;
+
+    this.model = electrolysisModel;
+
+    //Create the sky and ground.  Allow the sky and ground to go off the screen in case the window is larger than the sim aspect ratio
+    var arenaHeight = 376;
+    var divisionY = 368;
+    var rackHeight = height - arenaHeight;
+    this.addChild( new Rectangle( -width, -arenaHeight, width * 3, arenaHeight * 2, {
+      fill: new LinearGradient( 0, 0, 0, arenaHeight ).addColorStop( 0, '#02ace4' ).addColorStop( 1, '#cfecfc' )
+    } ) );
+    this.addChild( new Rectangle( -width, arenaHeight, width * 3, rackHeight * 3, {
+      fill: '#c59a5b'
+    } ) );
+
     // Reset All button
     var resetAllButton = new ResetAllButton( {
       listener: function() {
@@ -28,7 +53,7 @@ define( function( require ) {
       right:  this.layoutBounds.maxX - 10,
       bottom: this.layoutBounds.maxY - 10
     } );
-    this.addChild( resetAllButton );
+
   }
 
   return inherit( ScreenView, ElectrolysisScreenView, {
