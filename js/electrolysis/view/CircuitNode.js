c2f598eb848c0d3f67b1aba3731049067c4a53d8
Abdulsattar Mohammed <asattar.md@gmail.com>
2015-09-18 17:23:46 +0530

    Add toggling key

--- js/electrolysis/view/CircuitNode.js
+++ js/electrolysis/view/CircuitNode.js
@@ -4,6 +4,8 @@ define(function(require){
     var inherit = require('PHET_CORE/inherit');
     var Image = require('SCENERY/nodes/Image');
     var Rectangle = require('SCENERY/nodes/Rectangle');
+    var Circle = require('SCENERY/nodes/Circle');
+    var DownUpListener = require( 'SCENERY/input/DownUpListener' );
 
     var circuitImage = require('image!ELECTROLYSIS/circuit.svg');
 
@@ -15,6 +17,18 @@ define(function(require){
         model.electrolyteProperty.link(function(liquid) {
             rectangleNode.fill = liquid ? liquid.color : 'red';
         }.bind(this));
+
+        var key = new Circle(8, {x: 527, y: 171, fill: '#000', cursor: 'pointer', stroke: '#fff', lineWidth: 9});
+        key.addInputListener(new DownUpListener({
+            up: function() {
+                model.openProperty.set(!model.open);
+            }
+        }));
+        this.addChild(key);
+
+        model.openProperty.link(function(open) {
+            key.fill = open ? '#fff' : '#000';
+        });
 
         var image = new Image(circuitImage, {x: 0, y: 0});
         this.addChild(image);
