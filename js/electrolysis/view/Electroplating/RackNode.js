ece0ba2f0ac22e78df1c9f89597c4294aa805367
Prateek Srivastava <prateeks@thoughtworks.com>
2015-09-24 15:47:45 +0530

    Added the liquid and racknode for electroplating

---
+++ js/electrolysis/view/Electroplating/RackNode.js
@@ -0,0 +1,23 @@
+define(function(require){
+
+    var Node = require('SCENERY/nodes/Node');
+    var inherit = require('PHET_CORE/inherit');
+    var Image = require('SCENERY/nodes/Image');
+    var LiquidNode = require('ELECTROLYSIS/electrolysis/view/Electroplating/LiquidNode');
+
+    var rackImage = require('image!ELECTROLYSIS/rack.svg');
+
+    function RackNode(model, modelViewTransform) {
+        Node.call(this, {x: model.location.x, y: model.location.y});
+
+        var image = new Image(rackImage, {x: 0, y: 0});
+        this.addChild(image);
+
+        model.liquids.forEach(function(liquid) {
+            var liquidNode = new LiquidNode(liquid, modelViewTransform);
+            this.addChild(liquidNode);
+        }.bind(this));
+    }
+
+    return inherit(Node, RackNode);
+});
